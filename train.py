d85d2c64cc82f2b76c41592822538a9e1b5d599f
Ananya Jana <ananya.jana@gmail.com>
2020-08-25 09:04:54 -0400

    adding code to train

--- train.py
+++ train.py
@@ -1,52 +1,86 @@
-"""Test ImageNet pretrained DenseNet"""
-#from __future__ import print_function
-import  numpy as np
 import os
-
-import sys
+import  numpy as np
 import torch
-import torch.nn
-from multiprocessing.dummy import Pool as ThreadPool
-import random
-from medpy.io import load
-import numpy as np
-import argparse
-
-from PIL import Image
-from denseunet import denseUnet
-from skimage.transform import resize
+import torch.nn as nn
+#from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
+from multiprocessing.dummy import Pool as ThreadPool
+from medpy.io import load
+import argparse
+from denseunet import denseUnet
+from dataset import LiTSDataset
+from torch.autograd import Variable
+from sklearn import metrics
+from loss import *
+import time
 
-import nibabel as nib
-from nibabel.testing import data_path
 
-parser = argparse.ArgumentParser(description='Keras 2d denseunet Training')
+parser = argparse.ArgumentParser(description='PyTorch 2d denseunet Training')
 #  data folder
 parser.add_argument('-data', type=str, default='data/', help='test images')
-parser.add_argument('-save_path', type=str, default='Experiments/')
+parser.add_argument('-save_path', type=str, default='experiments/')
 #  other paras
 parser.add_argument('-b', type=int, default=40)
 parser.add_argument('-input_size', type=int, default=224)
 parser.add_argument('-model_weight', type=str, default='./model/densenet161_weights_tf.h5')
 parser.add_argument('-input_cols', type=int, default=3)
-
+parser.add_argument('--gpus', type=int, nargs='+', default=[0], help='GPUs for training')
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--save_dir', type=str, default='', help="specify the path for saving the model")
+parser.add_argument('--batch_size', type=int, default=1, help="specify the batch size")
 #  data augment
 parser.add_argument('-mean', type=int, default=48)
 parser.add_argument('-thread_num', type=int, default=14)
 args = parser.parse_args()
 
-#file_path = '/research/cbim/medical/aj611/experiments/data/ILSVRC12/val2/val/n04264628/ILSVRC2012_val_00001672.JPEG'
-file_path='../../data/ILSVRC12/val2/val/n04264628/ILSVRC2012_val_00001672.JPEG'
-img = Image.open(file_path)
-#trans = transforms.ToPILImage()
-trans = transforms.ToTensor()
-trans1 = transforms.Resize((224, 224))
-trans2 = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-im = trans2(trans(trans1(img)))
-print(im.size())
-im = im.unsqueeze(0)
-print(im.size())
+os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(x) for x in args.gpus)
+#writer = SummaryWriter()
 
 model = denseUnet(reduction=0.5)
-out = model(im)
-print(out)
+model = model.cuda()
+
+# run the model on multiple gpus
+model = torch.nn.DataParallel(model, device_ids=list(
+    range(torch.cuda.device_count()))).cuda()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, nesterov=True)
+
+data_transform = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.ToTensor()
+])
+
+train_set = LiTSDataset('{:s}/train.h5'.format(args.data), data_transform)
+test_set = LiTSDataset('{:s}/test.h5'.format(args.data), data_transform)
+
+idx_list = torch.randperm(len(train_set)).tolist()
+
+model.train()
+start = time.time()
+for i in range(0, len(idx_list), args.batch_size):
+    N = min(args.batch_size, len(idx_list) - i)
+    loss = 0
+    for k in range(N):
+        idx = idx_list[i + k]
+        images, masks = train_set[idx]
+        images = Variable(images.cuda())
+        #masks = masks.long()
+        masks = Variable(masks.long().cuda())        
+        output = model(images)
+        masks = masks.squeeze(1)
+        output = output.squeeze(1)
+        loss += criterion(output, masks)
+        print('loss: ', loss)
+    loss /= N
+    #losses.update(loss.item(), N)
+
+    # compute gradient and do SGD step
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # measure elapsed time
+    #batch_time.update(time.time() - end)
+    end = time.time()
+    print('batch Loss: ', loss)
